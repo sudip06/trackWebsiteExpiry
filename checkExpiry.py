39ca53ca8bfc589357f5b076876e22bfe788313f
Sudip Midya <midya.sudip@gmail.com>
2018-02-15 09:00:21 +0530

    minimal changes

--- checkExpiry.py
+++ checkExpiry.py
@@ -8,7 +8,6 @@ import numpy as np
 import os.path
 
 def calculateDaysToExpire(ExpiryDate):
-	print (ExpiryDate)
 	differenceTime=datetime.strptime(ExpiryDate,'%d/%m/%Y')-datetime.now()
 	return differenceTime.days
 
@@ -20,9 +19,10 @@ with open('fileList.dat') as f:
 		df=pd.read_csv('out.csv', sep='\t', encoding='utf-8', index_col='Domain')
 	else:
 		df=pd.DataFrame(columns=['Domain','Days to expire',"Expiry Date"])
+		df.set_index('Domain') 
 
 	for domain in content:
-		if (domain not in df.index) or np.isnan(df.at[domain,'Days to expire']):
+		if (domain not in df.index) or(np.isnan(df.at[domain,'Days to expire'])):
 			try:
 				w = whois.whois(domain)
 
@@ -34,6 +34,7 @@ with open('fileList.dat') as f:
 					w.expiration_date = w.expiration_date[0]
 					timedelta = w.expiration_date - now
 					days_to_expire = timedelta.days
+					domain_expiration_date=w.expiration_date
 				else:
 					domain_expiration_date = str(w.expiration_date.day) + '/' + str(w.expiration_date.month) + '/' + str(w.expiration_date.year)
 				timedelta = w.expiration_date - now
@@ -42,18 +43,16 @@ with open('fileList.dat') as f:
 			except (whois.parser.PywhoisError):
 					days_to_expire=np.nan
 					domain_expiration_date=np.nan
-                    
-			#df=df.append({'Domain':domain, 'Days to expire':days_to_expire, 'Expiry Date':domain_expiration_date},ignore_index=True)
 			df=df.append({'Domain':domain, 'Days to expire':days_to_expire, 'Expiry Date':domain_expiration_date}, ignore_index=True)
+			#df=df.append({'Domain':domain, 'Days to expire':days_to_expire, 'Expiry Date':domain_expiration_date})
+			#df=df.append({'Domain':domain, 'Days to expire':days_to_expire, 'Expiry Date':domain_expiration_date},ignore_index=True)
 			#df['Days to expire'].replace(999,np.nan)
 			#df['Expiry Date'].replace(999,np.nan)
 			#df.replace(int('999'),np.nan)
 			#df.replace(int('999'),np.nan)
 		#else:
+	df=df.reset_index()
 	df['Days to expire']=df['Expiry Date'].apply(calculateDaysToExpire)
-		#df.to_csv('out.csv', sep='\t', encoding='utf-8', index_label='Domain')
-	if os.path.exists('out.csv'):
-		os.remove('out.csv')
 	df.to_csv('out.csv', sep='\t', encoding='utf-8',index=False, columns=['Domain','Days to expire',"Expiry Date"])
 
 

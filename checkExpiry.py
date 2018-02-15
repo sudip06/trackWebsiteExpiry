f969021885a23284e7a1baf20dcbbe9a697468c4
Sudip Midya <midya.sudip@gmail.com>
2018-02-16 01:15:33 +0530

    some change

--- checkExpiry.py
+++ checkExpiry.py
@@ -30,30 +30,22 @@ with open('fileList.dat') as f:
 					days_to_expire=np.nan
 					domain_expiration_date=np.nan
 
-				if type(w.expiration_date) == list:
+				elif (type(w.expiration_date) == list):
 					w.expiration_date = w.expiration_date[0]
 					timedelta = w.expiration_date - now
 					days_to_expire = timedelta.days
 					domain_expiration_date=w.expiration_date
 				else:
 					domain_expiration_date = str(w.expiration_date.day) + '/' + str(w.expiration_date.month) + '/' + str(w.expiration_date.year)
-				timedelta = w.expiration_date - now
-				days_to_expire = timedelta.days
+				if not np.isnan(days_to_expire):
+					timedelta = w.expiration_date - now
+					days_to_expire = timedelta.days
 
 			except (whois.parser.PywhoisError):
 					days_to_expire=np.nan
 					domain_expiration_date=np.nan
-			df=df.append({'Domain':domain, 'Days to expire':days_to_expire, 'Expiry Date':domain_expiration_date}, ignore_index=True)
-			#df=df.append({'Domain':domain, 'Days to expire':days_to_expire, 'Expiry Date':domain_expiration_date})
-			#df=df.append({'Domain':domain, 'Days to expire':days_to_expire, 'Expiry Date':domain_expiration_date},ignore_index=True)
-			#df['Days to expire'].replace(999,np.nan)
-			#df['Expiry Date'].replace(999,np.nan)
-			#df.replace(int('999'),np.nan)
-			#df.replace(int('999'),np.nan)
-		#else:
+			#df=df.append({'Domain':domain, 'Days to expire':days_to_expire, 'Expiry Date':domain_expiration_date}, ignore_index=True)
+			df.loc[-1]=[domain, days_to_expire, domain_expiration_date]
 	df=df.reset_index()
 	df['Days to expire']=df['Expiry Date'].apply(calculateDaysToExpire)
 	df.to_csv('out.csv', sep='\t', encoding='utf-8',index=False, columns=['Domain','Days to expire',"Expiry Date"])
-
-
-
